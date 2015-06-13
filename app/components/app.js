4a01de0307f123e26430f8ed62b6795fa3a14a0e
Brian Fitch <brianmfitch@gmail.com>
2015-06-13 00:14:16 -0400

    test react-router

--- app/components/app.js
+++ app/components/app.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { RouteHandler } from "react-router";
 
-export default class App extends React.Component {
+export class App extends React.Component {
   render() {
-    return <div>Wazzzz appp ppppp</div>;
+    return (
+      <div>
+        <h1>App</h1>
+        <RouteHandler/>
+      </div>
+    );
+  }
+}
+
+export class About extends React.Component {
+  render() {
+    return (
+      <h1>About</h1>
+    );
+  }
+}
+
+export class Users extends React.Component {
+  render() {
+    return (
+      <h1>Users</h1>
+    );
+  }
+}
+
+export class NoMatch extends React.Component {
+  render() {
+    return (
+      <h1>No MATCH</h1>
+    );
   }
 }
--- app/main.js
+++ app/main.js
@@ -1,4 +1,18 @@
 import React from "react";
-import App from "./components/app.js";
+import Router from "react-router";
+import { App, About, Users, NoMatch } from "./components/app.js";
 
-React.render(<App />, document.getElementById("app"));
+const Route        = Router.Route;
+const DefaultRoute = Router.DefaultRoute;
+
+let routes = (
+  <Route path="/" handler={App}>
+    <Route path="about" handler={About}/>
+    <Route path="users" handler={Users}/>
+    <DefaultRoute handler={NoMatch}/>
+  </Route>
+);
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.getElementById("app"));
+});
