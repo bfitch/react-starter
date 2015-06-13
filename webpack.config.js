ab7082e092735a68b57085f77a3c1afb9cf8c0d6
Brian Fitch <brianmfitch@gmail.com>
2015-06-12 21:34:39 -0400

    init

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// run: webpack-dev-server --hot
+
+module.exports = {
+  entry: {
+    app: ['webpack/hot/dev-server', './app/main.js']
+  },
+  output: {
+    filename: 'dist/bundle.js'
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/, loader: "eslint-loader",
+        exclude: /(node_modules|bower_components)/
+      }
+    ],
+    loaders: [
+      { test: /\.js$/, loader: 'jsx-loader' },
+      {
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json']
+  }
+};
