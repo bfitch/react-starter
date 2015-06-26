09bfb0f777bbc373176991e7813bb13c42af8250
Brian Fitch <brianmfitch@gmail.com>
2015-06-26 08:52:22 -0400

    Lint double quotes in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   entry: {
-    app: ['webpack/hot/dev-server', './app/main.js']
+    app: ["webpack/hot/dev-server", "./app/main.js"]
   },
   output: {
-    filename: 'dist/bundle.js'
+    filename: "dist/bundle.js"
   },
   module: {
     preLoaders: [
@@ -15,10 +15,10 @@ module.exports = {
     loaders: [
       { test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel' }
+        loader: "babel" }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.json']
+    extensions: ["", ".js", ".json"]
   }
 };
